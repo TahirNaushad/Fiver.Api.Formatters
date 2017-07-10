adf3e03a68ddcc1f8268f3f3d574733a5802e498
Tahir Naushasd <t.naushad25@gmail.com>
2017-07-10 13:24:32 +0100

    XML formatters

---
+++ Fiver.Api.Formatters/Controllers/MoviesController.cs
@@ -0,0 +1,82 @@
+﻿using Fiver.Api.Formatters.Models.Movies;
+using Fiver.Api.Formatters.OtherLayers;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Fiver.Api.Formatters.Controllers
+{
+    [Route("movies")]
+    public class MoviesController : Controller
+    {
+        private readonly IMovieService service;
+
+        public MoviesController(IMovieService service)
+        {
+            this.service = service;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var model = service.GetMovies();
+
+            var outputModel = ToOutputModel(model);
+            return Ok(outputModel);
+        }
+
+        [HttpGet("{id}", Name = "GetMovie")]
+        public IActionResult Get(int id)
+        {
+            var model = service.GetMovie(id);
+
+            var outputModel = ToOutputModel(model);
+            return Ok(outputModel);
+        }
+
+        [HttpPost]
+        public IActionResult Create([FromBody]MovieInputModel inputModel)
+        {
+            var model = ToDomainModel(inputModel);
+            service.AddMovie(model);
+
+            var outputModel = ToOutputModel(model);
+            return CreatedAtRoute("GetMovie", new { id = outputModel.Id }, outputModel);
+        }
+        
+        #region " Mappings "
+
+        private MovieOutputModel ToOutputModel(Movie model)
+        {
+            if (model == null) return null;
+            return new MovieOutputModel
+            {
+                Id = model.Id,
+                Title = model.Title,
+                ReleaseYear = model.ReleaseYear,
+                Summary = model.Summary,
+                LastReadAt = DateTime.Now
+            };
+        }
+
+        private List<MovieOutputModel> ToOutputModel(List<Movie> model)
+        {
+            return model.Select(item => ToOutputModel(item))
+                        .ToList();
+        }
+
+        private Movie ToDomainModel(MovieInputModel inputModel)
+        {
+            return new Movie
+            {
+                Id = inputModel.Id,
+                Title = inputModel.Title,
+                ReleaseYear = inputModel.ReleaseYear,
+                Summary = inputModel.Summary
+            };
+        }
+        
+        #endregion
+    }
+}
--- Fiver.Api.Formatters/Startup.cs
+++ Fiver.Api.Formatters/Startup.cs
@@ -1,10 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using Fiver.Api.Formatters.OtherLayers;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc.Formatters;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 
@@ -12,26 +9,26 @@ namespace Fiver.Api.Formatters
 {
     public class Startup
     {
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
-        public void ConfigureServices(IServiceCollection services)
+        public void ConfigureServices(
+            IServiceCollection services)
         {
+            services.AddSingleton<IMovieService, MovieService>();
+
+            services.AddMvc(options =>
+            {
+                options.ReturnHttpNotAcceptable = true;
+                options.OutputFormatters.RemoveType<HttpNoContentOutputFormatter>();
+                options.InputFormatters.Add(new XmlSerializerInputFormatter());
+                options.OutputFormatters.Add(new XmlSerializerOutputFormatter());
+            });
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        public void Configure(
+            IApplicationBuilder app,
+            IHostingEnvironment env,
+            ILoggerFactory loggerFactory)
         {
-            loggerFactory.AddConsole();
-
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-
-            app.Run(async (context) =>
-            {
-                await context.Response.WriteAsync("Hello World!");
-            });
+            app.UseMvcWithDefaultRoute();
         }
     }
 }
